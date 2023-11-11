b2194ab2f0ba63089dc866e08d10ea9373283944
elpatrinum <meb-bak@outlook.fr>
2023-11-11 02:36:25 +0100

    hahaha

--- ft_put_u.c
+++ ft_put_u.c
@@ -12,25 +12,39 @@
 
 #include "ft_printf.h"
 
+static int	helper(unsigned int nb, int i, int j)
+{
+	j = 0;
+	j = ft_put_u(nb / 10);
+	if (j == -1)
+		return (-1);
+	else
+		i += j;
+	j = 0;
+	j = ft_put_u(nb % 10);
+	if (j == -1)
+		return (-1);
+	else
+		i += j;
+	return (i);
+}
+
 int	ft_put_u(unsigned int nb)
 {
 	int	i;
 	int	j;
+	int	x;
 
 	i = 0;
+	x = 0;
 	j = 0;
 	if (nb >= 10)
 	{
-		j = ft_put_u(nb / 10);
-		if (j == -1)
+		x = helper(nb, i, j);
+		if (x == -1)
 			return (-1);
 		else
-			i += j;
-		j = ft_put_u(nb % 10);
-		if (j == -1)
-			return (-1);
-		else
-			i += j;
+			i += x;
 	}
 	else
 	{
